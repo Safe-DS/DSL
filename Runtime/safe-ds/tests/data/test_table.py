a1f531fcb4167d6f97519aa782e65ddefbbe4488
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-11 15:07:16 +0100

    Changed Paths of test_table Data

--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -4,20 +4,20 @@ from safe_ds.data._table import Table
 
 
 def test_read_csv_valid():
-    table = Table.from_csv("test_table_read_csv.csv")
+    table = Table.from_csv("../tests/data/test_table_read_csv.csv")
     assert table.data["A"][0] == 1 and table.data["B"][0] == 2
 
 
 def test_read_csv_invalid():
     with pytest.raises(FileNotFoundError):
-        Table.from_csv("test_table_read_csv_invalid.csv")
+        Table.from_csv("../tests/data/test_table_read_csv_invalid.csv")
 
 
 def test_read_json_valid():
-    table = Table.from_csv("test_table_read_json.json")
+    table = Table.from_csv("../tests/data/test_table_read_json.json")
     assert table.data["A"][0] == 1 and table.data["B"][0] == 2
 
 
 def test_read_json_invalid():
     with pytest.raises(FileNotFoundError):
-        Table.from_csv("test_table_read_json_invalid.json")
+        Table.from_csv("../tests/data/test_table_read_json_invalid.json")
