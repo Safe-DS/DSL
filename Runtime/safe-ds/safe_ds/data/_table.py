3815b3d733048ae6ed742649bbbde3946c8b4633
Alexander Gréus <alexgreus51@gmail.com>
2022-11-11 14:42:04 +0100

    Added functions table.to_csv and table.to_json TODO: Add round test

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -1,3 +1,6 @@
+import os.path
+from pathlib import Path
+
 import pandas as pd
 
 
@@ -6,3 +9,21 @@ class Table:
 
     def __init__(self, data: pd.DataFrame):
         self.data = data
+
+    def to_json(self, path_to_file: str):
+        """
+        Write the data from the table into a json file.
+        :param path_to_file: The path as String to the output file. If the file does not exist it will be created.
+        If the file does already exist it will be overwritten.
+        """
+        Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
+        self.data.to_json(path_to_file)
+
+    def to_csv(self, path_to_file: str):
+        """
+        Write the data from the table into a csv file.
+        :param path_to_file: The path as String to the output file. If the file does not exist it will be created.
+        If the file does already exist it will be overwritten.
+        """
+        Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
+        self.data.to_csv(path_to_file)
