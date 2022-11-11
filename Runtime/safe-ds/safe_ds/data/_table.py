f20dadc5498f1b124cbed6db875f6f52358f2242
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-11 14:40:36 +0100

    feat: read_csv und read_json

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -6,3 +6,17 @@ class Table:
 
     def __init__(self, data: pd.DataFrame):
         self.data = data
+
+    @staticmethod
+    def from_json(path):
+        try:
+            return Table(pd.read_json(path))
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File \"{path}\" does not exist")
+
+    @staticmethod
+    def from_csv(path):
+        try:
+            return Table(pd.read_csv(path))
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File \"{path}\" does not exist")
