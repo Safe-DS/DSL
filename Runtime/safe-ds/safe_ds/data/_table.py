413eec66aed477f3739f9d2011e8d1ab24d3cd6f
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-11 14:55:16 +0100

    145: Documentation added

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -9,14 +9,28 @@ class Table:
 
     @staticmethod
     def from_json(path):
+        """
+        Reads data from a JSON file into a Table
+        Raises a FileNotFoundError if the specified file does not exist.
+        May raise other unspecified Errors.
+        :param path: Path to the file as String
+        :return: The Table read from the file
+        """
         try:
             return Table(pd.read_json(path))
-        except FileNotFoundError:
+        except FileNotFoundError as exc:
             raise FileNotFoundError(f"File \"{path}\" does not exist")
 
     @staticmethod
     def from_csv(path):
+        """
+        Reads data from a CSV file into a Table.
+        Raises a FileNotFoundError if the specified file does not exist.
+        May raise other unspecified Errors.
+        :param path: Path to the file as String
+        :return: The Table read from the file
+        """
         try:
             return Table(pd.read_csv(path))
-        except FileNotFoundError:
+        except FileNotFoundError as exc:
             raise FileNotFoundError(f"File \"{path}\" does not exist")
