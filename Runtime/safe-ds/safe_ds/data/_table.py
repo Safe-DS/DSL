1bbf98cc33256f768e2cc751067efca75b0ad337
Andreas <s6asgerh@uni-bonn.de>
2022-11-11 15:12:21 +0100

    Added the get by row Function

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -6,3 +6,14 @@ class Table:
 
     def __init__(self, data: pd.DataFrame):
         self.data = data
+
+    def get_row_by_index(self, index):
+        """
+        raise ValueError  if the index doesn't exist
+        :param index:
+        :return: the row for the given Index
+        """
+        if len(self.data.index)-1 < index | index < 0:
+            raise ValueError
+        return self.data.iloc[[index]]
+
