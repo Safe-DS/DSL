e8ac39d85d638a5ab4ee2c9893fad472f4f11485
Marvjowa <Marvjowa@users.noreply.github.com>
2022-11-18 15:14:59 +0000

    style: apply automated linter fixes

--- Runtime/safe-ds/safe_ds/data/_column.py
+++ Runtime/safe-ds/safe_ds/data/_column.py
@@ -9,21 +9,21 @@ class Column:
     def get_value_by_position(self, index: int):
         """Returns column value at specified index, starting at 0.
 
-                Parameters
-                ----------
-                index : int
-                    Index of requested element as integer.
+        Parameters
+        ----------
+        index : int
+            Index of requested element as integer.
 
-                Returns
-                -------
-                value
-                    Value at index in column.
+        Returns
+        -------
+        value
+            Value at index in column.
 
-                Raises
-                ------
-                IndexOutOfBoundsError
-                    If the given index does not exist in the column.
-                """
+        Raises
+        ------
+        IndexOutOfBoundsError
+            If the given index does not exist in the column.
+        """
         if index < 0 or index >= self._data.size:
             raise IndexOutOfBoundsError(index)
 
