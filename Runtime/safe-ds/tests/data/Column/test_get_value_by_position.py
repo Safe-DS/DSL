7e74264ca26cc42cb6efc1aaf8827ff835b24d38
Marvin Walter <walter@zbmed.de>
2022-11-18 15:53:22 +0100

    Access value of column by numerical index #150

---
+++ Runtime/safe-ds/tests/data/Column/test_get_value_by_position.py
@@ -0,0 +1,17 @@
+import pytest
+import pandas as pd
+from safe_ds.data import Column
+from safe_ds.exceptions import IndexOutOfBoundsError
+
+def test_value_by_position_valid():
+    column = Column(pd.Series([0, '1']))
+    assert column.get_value_by_position(0) == 0
+    assert column.get_value_by_position(1) == '1'
+
+def test_value_by_position_invalid():
+    column = Column(pd.Series([0, '1']))
+    with pytest.raises(IndexOutOfBoundsError):
+        column.get_value_by_position(-1)
+
+    with pytest.raises(IndexOutOfBoundsError):
+        column.get_value_by_position(2)
