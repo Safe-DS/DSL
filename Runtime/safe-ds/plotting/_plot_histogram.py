05077de091c9dfa706280be0616b88657645598c
Marsmaennchen221 <Marsmaennchen221@users.noreply.github.com>
2022-11-18 15:28:26 +0000

    style: apply automated linter fixes

--- Runtime/safe-ds/plotting/_plot_histogram.py
+++ Runtime/safe-ds/plotting/_plot_histogram.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from safe_ds.data import Column
 
 
 class PlotHistogram:
-
     @staticmethod
     def plot_histogram(column: Column):
         """
@@ -18,7 +16,8 @@ class PlotHistogram:
         """
         # noinspection PyProtectedMember
         ax = sns.histplot(data=column._data)
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45,
-                           horizontalalignment="right")  # rotate the labels of the x Axis to prevent the chance of overlapping of the labels
+        ax.set_xticklabels(
+            ax.get_xticklabels(), rotation=45, horizontalalignment="right"
+        )  # rotate the labels of the x Axis to prevent the chance of overlapping of the labels
         plt.tight_layout()
         plt.show()
