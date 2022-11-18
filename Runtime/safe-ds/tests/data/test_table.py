6e0a83e9a8dc73d762c2d779c1cd3643582cb8f4
Alexander Gréus <alexgreus51@gmail.com>
2022-11-18 14:32:38 +0100

    Fixed the tests

--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -13,20 +13,15 @@ def test_read_csv_valid():
 
 def test_get_column_by_name_valid():
     table = Table(pd.DataFrame(data={'col1': ["col1_1"], 'col2': ["col2_1"]}))
-    assert isinstance(table.get_column_by_name("col1"), Column) and table.get_column_by_name("col1").data[0] == \
+    assert isinstance(table.get_column_by_name("col1"), Column) and table.get_column_by_name("col1")._data[0] == \
            pd.Series(
-               data=["col1_1"])[0] and table.get_column_by_name("col1").data[0] == "col1_1"
+               data=["col1_1"])[0] and table.get_column_by_name("col1")._data[0] == "col1_1"
 
 
 def test_get_column_by_name_invalid():
     with pytest.raises(ColumnNameError):
         table = Table(pd.DataFrame(data={'col1': ["col1_1"], 'col2': ["col2_1"]}))
         table.get_column_by_name("col3")
-
-
-def test_read_csv_valid():
-    table = Table.from_csv("test_table_read_csv.csv")
-    assert table.data["A"][0] == 1 and table.data["B"][0] == 2
 
 
 def test_read_csv_invalid():
