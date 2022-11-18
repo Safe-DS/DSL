70450e4e24aab9e879e26a2d92777f66a5e6a739
Alexander Gréus <alexgreus51@gmail.com>
2022-11-18 15:39:09 +0100

    Added Megalinter corrections

--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -87,21 +87,21 @@ def test_rename_invalid(name_from, name_to, error):
 
 def test_write_and_read_json_valid():
     table = Table(pd.DataFrame(data={"col1": ["col1_1"], "col2": ["col2_1"]}))
-    tmp_table_file = NamedTemporaryFile()
-    tmp_table_file.close()
-    with open(tmp_table_file.name, 'w') as tmp_file:
-        table.to_json(tmp_file.name)
-    with open(tmp_table_file.name, 'r') as tmp_file:
-        table_r = Table.from_json(tmp_file.name)
+    with NamedTemporaryFile() as tmp_table_file:
+        tmp_table_file.close()
+        with open(tmp_table_file.name, 'w', encoding="utf-8") as tmp_file:
+            table.to_json(tmp_file.name)
+        with open(tmp_table_file.name, 'r', encoding="utf-8") as tmp_file:
+            table_r = Table.from_json(tmp_file.name)
     assert table._data.equals(table_r._data)
 
 
 def test_write_and_read_csv_valid():
     table = Table(pd.DataFrame(data={"col1": ["col1_1"], "col2": ["col2_1"]}))
-    tmp_table_file = NamedTemporaryFile()
-    tmp_table_file.close()
-    with open(tmp_table_file.name, 'w') as tmp_file:
-        table.to_csv(tmp_file.name)
-    with open(tmp_table_file.name, 'r') as tmp_file:
-        table_r = Table.from_csv(tmp_file.name)
+    with NamedTemporaryFile() as tmp_table_file:
+        tmp_table_file.close()
+        with open(tmp_table_file.name, 'w', encoding="utf-8") as tmp_file:
+            table.to_csv(tmp_file.name)
+        with open(tmp_table_file.name, 'r', encoding="utf-8") as tmp_file:
+            table_r = Table.from_csv(tmp_file.name)
     assert table._data.equals(table_r._data)
