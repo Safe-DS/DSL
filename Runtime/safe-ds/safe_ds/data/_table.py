914e1c01f6e3a801d82adf1f9873ba724bf757d7
Alexander Gréus <alexgreus51@gmail.com>
2022-11-18 14:56:40 +0100

    Changed docstrings and changed to TmpFiles for tests

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -75,10 +75,11 @@ class Table:
     def to_json(self, path_to_file: str):
         """
         Write the data from the table into a json file.
+        If the file and/or the directories do not exist they will be created. If the file does already exist it will be overwritten.
 
         Parameters
         ----------
-        path_to_file : The path as String to the output file. If the file does not exist it will be created. If the file does already exist it will be overwritten.
+        path_to_file : The path as String to the output file.
         """
         Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
         self._data.to_json(path_to_file)
@@ -86,10 +87,11 @@ class Table:
     def to_csv(self, path_to_file: str):
         """
         Write the data from the table into a csv file.
+        If the file and/or the directories do not exist they will be created. If the file does already exist it will be overwritten.
 
         Parameters
         ----------
-        path_to_file : The path as String to the output file. If the file does not exist it will be created. If the file does already exist it will be overwritten.
+        path_to_file : The path as String to the output file.
         """
         Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
         self._data.to_csv(path_to_file, index=False)
--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -1,7 +1,8 @@
-import os
+from tempfile import NamedTemporaryFile
 
 import pandas as pd
 import pytest
+
 from safe_ds.data import Table
 
 
@@ -27,17 +28,21 @@ def test_read_json_invalid():
 
 def test_write_and_read_json_valid():
     table = Table(pd.DataFrame(data={"col1": ["col1_1"], "col2": ["col2_1"]}))
-    if os.path.isfile("tests/resources/test_table_write_and_read_valid.json"):
-        os.remove("tests/resources/test_table_write_and_read_valid.json")
-    table.to_json("tests/resources/test_table_write_and_read_valid.json")
-    table_r = Table.from_json("tests/resources/test_table_write_and_read_valid.json")
+    tmp_table_file = NamedTemporaryFile()
+    tmp_table_file.close()
+    with open(tmp_table_file.name, 'w') as tmp_file:
+        table.to_json(tmp_file.name)
+    with open(tmp_table_file.name, 'r') as tmp_file:
+        table_r = Table.from_json(tmp_file.name)
     assert table._data.equals(table_r._data)
 
 
 def test_write_and_read_csv_valid():
     table = Table(pd.DataFrame(data={"col1": ["col1_1"], "col2": ["col2_1"]}))
-    if os.path.isfile("tests/resources/test_table_write_and_read_valid.csv"):
-        os.remove("tests/resources/test_table_write_and_read_valid.csv")
-    table.to_csv("tests/resources/test_table_write_and_read_valid.csv")
-    table_r = Table.from_csv("tests/resources/test_table_write_and_read_valid.csv")
+    tmp_table_file = NamedTemporaryFile()
+    tmp_table_file.close()
+    with open(tmp_table_file.name, 'w') as tmp_file:
+        table.to_csv(tmp_file.name)
+    with open(tmp_table_file.name, 'r') as tmp_file:
+        table_r = Table.from_csv(tmp_file.name)
     assert table._data.equals(table_r._data)
