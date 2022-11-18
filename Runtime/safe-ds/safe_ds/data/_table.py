4e70db7095c54f1a715d0008a9d7200966015b99
Alexander Gréus <alexgreus51@gmail.com>
2022-11-18 13:54:27 +0100

    Added Tests and changed docstring to numpydoc format

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -75,17 +75,21 @@ class Table:
     def to_json(self, path_to_file: str):
         """
         Write the data from the table into a json file.
-        :param path_to_file: The path as String to the output file. If the file does not exist it will be created.
-        If the file does already exist it will be overwritten.
+
+        Parameters
+        ----------
+        path_to_file : The path as String to the output file. If the file does not exist it will be created. If the file does already exist it will be overwritten.
         """
         Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
-        self.data.to_json(path_to_file)
+        self._data.to_json(path_to_file)
 
     def to_csv(self, path_to_file: str):
         """
         Write the data from the table into a csv file.
-        :param path_to_file: The path as String to the output file. If the file does not exist it will be created.
-        If the file does already exist it will be overwritten.
+
+        Parameters
+        ----------
+        path_to_file : The path as String to the output file. If the file does not exist it will be created. If the file does already exist it will be overwritten.
         """
         Path(os.path.dirname(path_to_file)).mkdir(parents=True, exist_ok=True)
-        self.data.to_csv(path_to_file)
+        self._data.to_csv(path_to_file, index=False)
--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -1,4 +1,8 @@
+import os
+
+import pandas as pd
 import pytest
+
 from safe_ds.data import Table
 
 
@@ -20,3 +24,21 @@ def test_read_json_valid():
 def test_read_json_invalid():
     with pytest.raises(FileNotFoundError):
         Table.from_json("tests/resources/test_table_read_json_invalid.json")
+
+
+def test_write_and_read_json_valid():
+    table = Table(pd.DataFrame(data={'col1': ["col1_1"], 'col2': ["col2_1"]}))
+    if os.path.isfile("tests/resources/test_table_write_and_read_valid.json"):
+        os.remove("tests/resources/test_table_write_and_read_valid.json")
+    table.to_json("tests/resources/test_table_write_and_read_valid.json")
+    table_r = Table.from_json("tests/resources/test_table_write_and_read_valid.json")
+    assert table._data.equals(table_r._data)
+
+
+def test_write_and_read_csv_valid():
+    table = Table(pd.DataFrame(data={'col1': ["col1_1"], 'col2': ["col2_1"]}))
+    if os.path.isfile("tests/resources/test_table_write_and_read_valid.csv"):
+        os.remove("tests/resources/test_table_write_and_read_valid.csv")
+    table.to_csv("tests/resources/test_table_write_and_read_valid.csv")
+    table_r = Table.from_csv("tests/resources/test_table_write_and_read_valid.csv")
+    assert table._data.equals(table_r._data)
