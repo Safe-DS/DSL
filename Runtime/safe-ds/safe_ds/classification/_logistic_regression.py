5bfdfc62b6de6a33e79af6eb03477635adf5a92f
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-25 15:34:34 +0100

    refactor: Move common methods to _util package

--- Runtime/safe-ds/safe_ds/classification/_logistic_regression.py
+++ Runtime/safe-ds/safe_ds/classification/_logistic_regression.py
@@ -1,7 +1,8 @@
-from safe_ds.data import SupervisedDataset, Table
-from safe_ds.exceptions import LearningError, PredictionError
-from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LogisticRegression as sk_LogisticRegression
+
+# noinspection PyProtectedMember
+import safe_ds._util._util_sklearn
+from safe_ds.data import SupervisedDataset, Table
 
 
 # noinspection PyProtectedMember
@@ -28,15 +29,7 @@ class LogisticRegression:
         LearningError
             if the supervised dataset contains invalid values or if the training failed
         """
-        try:
-            self._clf.fit(
-                supervised_dataset.feature_vectors._data,
-                supervised_dataset.target_values._data,
-            )
-        except ValueError as exception:
-            raise LearningError(str(exception)) from exception
-        except Exception as exception:
-            raise LearningError(None) from exception
+        safe_ds._util._util_sklearn.fit(self._clf, supervised_dataset)
 
     def predict(self, dataset: Table) -> Table:
         """
@@ -57,18 +50,4 @@ class LogisticRegression:
         PredictionError
             if predicting with the given dataset failed
         """
-        try:
-            predicted_target_vector = self._clf.predict(dataset._data)
-            result_set = dataset._data.copy(deep=True)
-            if "target_predictions" in result_set.columns:
-                raise ValueError(
-                    "Dataset already contains 'target_predictions' column. Please rename this column"
-                )
-            result_set["target_predictions"] = predicted_target_vector
-            return Table(result_set)
-        except NotFittedError as exception:
-            raise PredictionError("The model was not trained") from exception
-        except ValueError as exception:
-            raise PredictionError(str(exception)) from exception
-        except Exception as exception:
-            raise PredictionError(None) from exception
+        return safe_ds._util._util_sklearn.predict(self._clf, dataset)
--- Runtime/safe-ds/safe_ds/regression/_linear_regression.py
+++ Runtime/safe-ds/safe_ds/regression/_linear_regression.py
@@ -1,7 +1,8 @@
-from safe_ds.data import SupervisedDataset, Table
-from safe_ds.exceptions import LearningError, PredictionError
-from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LinearRegression as sk_LinearRegression
+
+# noinspection PyProtectedMember
+import safe_ds._util._util_sklearn
+from safe_ds.data import SupervisedDataset, Table
 
 
 # noinspection PyProtectedMember
@@ -12,7 +13,7 @@ class LinearRegression:
     """
 
     def __init__(self):
-        self._clf = sk_LinearRegression(n_jobs=-1)
+        self._regression = sk_LinearRegression(n_jobs=-1)
 
     def fit(self, supervised_dataset: SupervisedDataset):
         """
@@ -28,15 +29,7 @@ class LinearRegression:
         LearningError
             if the supervised dataset contains invalid values or if the training failed
         """
-        try:
-            self._clf.fit(
-                supervised_dataset.feature_vectors._data,
-                supervised_dataset.target_values._data,
-            )
-        except ValueError as exception:
-            raise LearningError(str(exception)) from exception
-        except Exception as exception:
-            raise LearningError(None) from exception
+        safe_ds._util._util_sklearn.fit(self._regression, supervised_dataset)
 
     def predict(self, dataset: Table) -> Table:
         """
@@ -57,18 +50,4 @@ class LinearRegression:
         PredictionError
             if predicting with the given dataset failed
         """
-        try:
-            predicted_target_vector = self._clf.predict(dataset._data)
-            result_set = dataset._data.copy(deep=True)
-            if "target_predictions" in result_set.columns:
-                raise ValueError(
-                    "Dataset already contains 'target_predictions' column. Please rename this column"
-                )
-            result_set["target_predictions"] = predicted_target_vector
-            return Table(result_set)
-        except NotFittedError as exception:
-            raise PredictionError("The model was not trained") from exception
-        except ValueError as exception:
-            raise PredictionError(str(exception)) from exception
-        except Exception as exception:
-            raise PredictionError(None) from exception
+        return safe_ds._util._util_sklearn.predict(self._regression, dataset)
