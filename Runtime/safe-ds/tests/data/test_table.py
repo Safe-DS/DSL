d9305429f02c9ca4b9a6d5a617e5481674f01eb2
Marvin Walter <walter@zbmed.de>
2022-11-25 12:40:24 +0100

    changed invalid test for Linter

--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -141,6 +141,7 @@ def test_filter_rows_valid():
     result_table = table.filter_rows(lambda x: x['col1'] <= x['col2'])
     assert result_table._data['col1'][1] == 3 and result_table._data.size == 4
 
+
 def test_filter_rows_invalid():
     table = Table(pd.DataFrame(data={'col1': [1, 2, 3], 'col2': [1, 1, 4]}))
     with pytest.raises(TypeError):
