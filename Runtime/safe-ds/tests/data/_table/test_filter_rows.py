5898154a50695f57debac396a1efd7d913705ef0
Lars Reimann <mail@larsreimann.com>
2022-12-02 14:39:06 +0100

    test: fix failing tests

--- Runtime/safe-ds/tests/data/_table/test_filter_rows.py
+++ Runtime/safe-ds/tests/data/_table/test_filter_rows.py
@@ -6,7 +6,7 @@ from safe_ds.data import Table
 def test_filter_rows_valid():
     table = Table(pd.DataFrame(data={"col1": [1, 2, 1], "col2": [1, 2, 4]}))
     result_table = table.filter_rows(
-        lambda row: row.get_value_by_column_name("col1") == 1
+        lambda row: row.get_value("col1") == 1
     )
     assert (
         result_table._data["col1"][0] == 1
@@ -15,6 +15,7 @@ def test_filter_rows_valid():
     )
 
 
+# noinspection PyTypeChecker
 def test_filter_rows_invalid():
     table = Table(pd.DataFrame(data={"col1": [1, 2, 3], "col2": [1, 1, 4]}))
     with pytest.raises(TypeError):
