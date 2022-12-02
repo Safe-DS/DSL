8f24448401d0df926f32b06a3facde762f950ee6
GideonKoenig <gideon.koenig@online.de>
2022-12-02 10:40:39 +0100

    feat: Add method to determine idness of column feat: Add test for idness of column method

--- Runtime/safe-ds/safe_ds/exceptions/__init__.py
+++ Runtime/safe-ds/safe_ds/exceptions/__init__.py
@@ -2,5 +2,6 @@ from ._data_exceptions import (
     ColumnNameDuplicateError,
     ColumnNameError,
     IndexOutOfBoundsError,
+    ColumnSizeError
 )
 from ._learning_exceptions import LearningError, PredictionError
