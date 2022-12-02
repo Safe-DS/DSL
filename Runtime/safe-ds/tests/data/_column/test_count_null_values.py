592c5c51986cdab4d806c7461642559e6676acaa
Marvjowa <Marvjowa@users.noreply.github.com>
2022-12-02 11:51:24 +0000

    style: apply automated linter fixes

--- Runtime/safe-ds/tests/data/_column/test_count_null_values.py
+++ Runtime/safe-ds/tests/data/_column/test_count_null_values.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 from safe_ds.data import Table
 
+
 def test_count_null_values_valid():
-    table = Table(pd.DataFrame(data={"col1": [1, 2, 3, 4, 5], "col2": [None, None, 1, np.nan, np.nan]}))
+    table = Table(
+        pd.DataFrame(
+            data={"col1": [1, 2, 3, 4, 5], "col2": [None, None, 1, np.nan, np.nan]}
+        )
+    )
     empty_table = Table(pd.DataFrame(data={"col1": []}))
     column1 = table.get_column_by_name("col1")
     column2 = table.get_column_by_name("col2")
