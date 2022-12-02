4b5f0b24762953875299942fe904f292c0808d94
GideonKoenig <gideon.koenig@online.de>
2022-12-02 14:15:53 +0100

    refactor: Use parametrized test for to_columns method.

--- Runtime/safe-ds/tests/data/_column/test_to_columns.py
+++ Runtime/safe-ds/tests/data/_column/test_to_columns.py
@@ -1,17 +1,18 @@
 import numpy as np
 import pandas as pd
+import pytest
+
 from safe_ds.data import Column, ColumnType, Table
 
 
-def test_to_columns():
+@pytest.mark.parametrize(
+    "values, name, type, index",
+    [([1, 4], "A", "int64", 0), ([2, 5], "B", "int64", 1)],
+)
+def test_to_columns(values: list[int], name: str, type: str, index: int):
     table = Table.from_csv("tests/resources/test_column_table.csv")
-
-    columns_expected: list[Column] = [
-        Column(pd.Series([1, 4]), "A", ColumnType.from_numpy_dtype(np.dtype("int64"))),
-        Column(pd.Series([2, 5]), "B", ColumnType.from_numpy_dtype(np.dtype("int64"))),
-    ]
-
     columns_list: list[Column] = table.to_columns()
 
-    for column_table, column_expected in zip(columns_list, columns_expected):
-        assert column_table._data.equals(column_expected._data)
+    column_expected: Column = Column(pd.Series(values, name=name), name, ColumnType.from_numpy_dtype(np.dtype(type)))
+
+    assert column_expected._data.equals(columns_list[index]._data)
