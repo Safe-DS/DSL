afb9b7b31170b667159887609a2dc9a9e4ac80dd
Alexander Gréus <alexgreus51@gmail.com>
2022-12-02 13:40:33 +0100

    Changed accuracy tests to use their specific ColumnType

--- Runtime/safe-ds/tests/classification/metrics/_accuracy/test_accuracy.py
+++ Runtime/safe-ds/tests/classification/metrics/_accuracy/test_accuracy.py
@@ -1,18 +1,20 @@
 import pandas as pd
+
 from safe_ds.classification.metrics import accuracy
-from safe_ds.data import Column, ColumnType
+from safe_ds.data import Column
+from safe_ds.data._column_type import IntColumnType, StringColumnType
 
 
 def test_accuracy():
     c1 = Column(
         pd.Series(data=[1, 2, 3, 4]),
         "TestColumn1",
-        ColumnType.from_numpy_dtype(pd.Series(data=[1, 2, 3, 4]).dtype),
+        IntColumnType,
     )
     c2 = Column(
         pd.Series(data=[1, 2, 3, 3]),
         "TestColumn2",
-        ColumnType.from_numpy_dtype(pd.Series(data=[1, 2, 3, 3]).dtype),
+        IntColumnType,
     )
     assert accuracy(c1, c2) == 0.75
 
@@ -21,11 +23,11 @@ def test_accuracy_different_types():
     c1 = Column(
         pd.Series(data=["1", "2", "3", "4"]),
         "TestColumn1",
-        ColumnType.from_numpy_dtype(pd.Series(data=[1, 2, 3, 4]).dtype),
+        StringColumnType,
     )
     c2 = Column(
         pd.Series(data=[1, 2, 3, 3]),
         "TestColumn2",
-        ColumnType.from_numpy_dtype(pd.Series(data=[1, 2, 3, 3]).dtype),
+        IntColumnType,
     )
     assert accuracy(c1, c2) == 0.0
