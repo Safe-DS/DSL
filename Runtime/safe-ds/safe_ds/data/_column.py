98c8269bfab0c003a394aa621c4e105ca16e9514
GideonKoenig <gideon.koenig@online.de>
2022-12-02 16:18:33 +0100

    fix: testerrors and linter

--- Runtime/safe-ds/safe_ds/data/_column.py
+++ Runtime/safe-ds/safe_ds/data/_column.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
-from safe_ds.exceptions import IndexOutOfBoundsError, ColumnSizeError
-from ._column_type import ColumnType
+
 from typing import Any
 
 import pandas as pd
+from safe_ds.exceptions import ColumnSizeError, IndexOutOfBoundsError
+
+from ._column_type import ColumnType
 
 
 class Column:
--- Runtime/safe-ds/tests/regression/metrics/test_mean_squared_error.py
+++ Runtime/safe-ds/tests/regression/metrics/test_mean_squared_error.py
@@ -13,11 +13,11 @@ from safe_ds.regression.metrics import mean_squared_error
         ([1, 2], [1, 2], 0),
         ([0, 0], [1, 1], 1),
         ([1, 1, 1], [2, 2, 11], 34)
-    ]
+    ],
 )
 def test_mean_squared_error_valid(actual: list[float], expected: list[float], result: float):
-    actual_column: Column = Column(pd.Series([actual]), "actual", ColumnType.from_numpy_dtype(np.dtype(float)))
-    expected_column: Column = Column(pd.Series([expected]), "expected", ColumnType.from_numpy_dtype(np.dtype(float)))
+    actual_column: Column = Column(pd.Series(actual), "actual", ColumnType.from_numpy_dtype(np.dtype(float)))
+    expected_column: Column = Column(pd.Series(expected), "expected", ColumnType.from_numpy_dtype(np.dtype(float)))
     assert mean_squared_error(actual_column, expected_column) == result
 
 
@@ -25,8 +25,8 @@ def test_mean_squared_error_valid(actual: list[float], expected: list[float], re
     "actual, expected, error",
     [(["A", "B"], [1, 2], TypeError), ([1, 2], ["A", "B"], TypeError), ([1, 2, 3], [1, 2], ColumnLengthMismatchError)],
 )
-def test_mean_squared_error_invalid(actual, expected, error):
-    actual_column = Column(pd.Series([actual]), "actual", ColumnType.from_numpy_dtype(pd.Series([actual]).dtype))
-    expected_column = Column(pd.Series([actual]), "expected", ColumnType.from_numpy_dtype(pd.Series([expected]).dtype))
-    with pytest.raises(TypeError):
+def test_mean_squared_error_invalid(actual: list[str | int], expected: list[str | int], error: type(Exception)):
+    actual_column = Column(pd.Series(actual), "actual", ColumnType.from_numpy_dtype(pd.Series(actual).dtype))
+    expected_column = Column(pd.Series(expected), "expected", ColumnType.from_numpy_dtype(pd.Series(expected).dtype))
+    with pytest.raises(error):
         mean_squared_error(actual_column, expected_column)
