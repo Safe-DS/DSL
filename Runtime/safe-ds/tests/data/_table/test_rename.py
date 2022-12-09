11df1c4f1a75235f49846c7058e5d6c44f6a7485
Gerhardsa0 <andreasgerhards@t-online.de>
2022-12-09 15:28:03 +0100

    changed Linter errors (:

--- Runtime/safe-ds/tests/data/_table/test_rename.py
+++ Runtime/safe-ds/tests/data/_table/test_rename.py
@@ -9,7 +9,7 @@ from safe_ds.exceptions import ColumnNameDuplicateError, ColumnNameError
     "name_from, name_to, column_one, column_two",
     [("A", "D", "D", "B"), ("A", "A", "A", "B")],
 )
-def test_rename_valid(name_from: str, name_to: str, column_one: Column, column_two) -> None:
+def test_rename_valid(name_from: str, name_to: str, column_one: Column, column_two: Column) -> None:
     table: Table = Table.from_csv("tests/resources/test_table_read_csv.csv")
     renamed_table = table.rename_column(name_from, name_to)
     assert renamed_table._data.columns[0] == column_one
