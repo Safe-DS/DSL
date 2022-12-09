aab3ae3ec8ccf70441ba22892df9c880f09d1db7
GideonKoenig <gideon.koenig@online.de>
2022-12-09 14:49:21 +0100

    fix: Satisfy linter

--- Runtime/safe-ds/tests/data/_table/test_table_add_column.py
+++ Runtime/safe-ds/tests/data/_table/test_table_add_column.py
@@ -6,24 +6,24 @@ from safe_ds.exceptions import ColumnNameDuplicateError, ColumnSizeError
 
 
 def test_table_add_column_valid() -> None:
-    input = Table.from_csv("tests/resources/test_table_add_column_valid_input.csv")
+    input_table = Table.from_csv("tests/resources/test_table_add_column_valid_input.csv")
     expected = Table.from_csv("tests/resources/test_table_add_column_valid_output.csv")
     column = Column(pd.Series(['a', 'b', 'c']), "C", StringColumnType())
 
-    result = input.add_column(column)
+    result = input_table.add_column(column)
     assert expected == result
 
 
 @pytest.mark.parametrize(
-    "column, column_name, error",
+    "column_values, column_name, error",
     [
         (['a', 'b', 'c'], "B", ColumnNameDuplicateError),
         (['a', 'b'], "C", ColumnSizeError),
     ],
 )
-def test_table_add_column_(column: list[str], column_name: str, error: type[Exception]) -> None:
-    input = Table.from_csv("tests/resources/test_table_add_column_valid_input.csv")
-    column = Column(pd.Series(column), column_name, StringColumnType())
+def test_table_add_column_(column_values: list[str], column_name: str, error: type[Exception]) -> None:
+    input_table = Table.from_csv("tests/resources/test_table_add_column_valid_input.csv")
+    column = Column(pd.Series(column_values), column_name, StringColumnType())
 
     with pytest.raises(error):
-        result = input.add_column(column)
+        input_table.add_column(column)
