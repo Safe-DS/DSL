2adb9d25b0bf8ce1e05dbfc52d6cf1f35a8fda47
WinPlay02 <WinPlay02@users.noreply.github.com>
2022-12-16 13:31:49 +0000

    style: apply automated linter fixes

--- Runtime/safe-ds/tests/data/_row/test_has_column.py
+++ Runtime/safe-ds/tests/data/_row/test_has_column.py
@@ -11,4 +11,3 @@ def test_has_column_negative() -> None:
     table = Table.from_csv("tests/resources/test_table_has_column.csv")
     row = table.to_rows()[0]
     assert not row.has_column("C")
-
--- Runtime/safe-ds/tests/data/_table/test_has_column.py
+++ Runtime/safe-ds/tests/data/_table/test_has_column.py
@@ -9,4 +9,3 @@ def test_has_column_positive() -> None:
 def test_has_column_negative() -> None:
     table = Table.from_csv("tests/resources/test_table_has_column.csv")
     assert not table.has_column("C")
-
