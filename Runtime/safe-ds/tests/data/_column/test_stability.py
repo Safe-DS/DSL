685bcfb4e3f08ef074a5f0af16369d5a2f2070bf
WinPlay02 <winplay02_gh@woberlaender.de>
2022-12-16 13:05:17 +0100

    fix: Test invalid case

--- Runtime/safe-ds/tests/data/_column/test_stability.py
+++ Runtime/safe-ds/tests/data/_column/test_stability.py
@@ -1,8 +1,10 @@
 import typing
 
+import numpy as np
 import pandas as pd
 import pytest
-from safe_ds.data import Column
+from safe_ds.data import Column, IntColumnType
+from safe_ds.exceptions import ColumnSizeError
 
 
 @pytest.mark.parametrize(
@@ -16,3 +18,9 @@ from safe_ds.data import Column
 def test_table_stability(values: list[typing.Any], expected: float) -> None:
     column = Column(pd.Series(values), "A")
     assert column.stability() == expected
+
+
+def test_table_error() -> None:
+    column = Column(pd.Series([], dtype=np.dtype("float64")), "A")  # Fix warning against unknown type
+    with pytest.raises(ColumnSizeError):
+        column.stability()
