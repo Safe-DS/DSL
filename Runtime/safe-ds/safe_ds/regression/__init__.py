293194983fbbe8fd623c4f49c560b82ec69f4c69
SmiteDeluxe <smitedeluxe@gmail.com>
2022-12-23 11:58:17 +0100

    Auto stash before merge of "192-k-nearest-neighbors-for-classification-and-regression" and "origin/main"

--- Runtime/safe-ds/safe_ds/regression/__init__.py
+++ Runtime/safe-ds/safe_ds/regression/__init__.py
@@ -4,3 +4,4 @@ from ._lasso_regression import LassoRegression
 from ._linear_regression import LinearRegression
 from ._random_forest import RandomForest
 from ._ridge_regression import RidgeRegression
+from ._k_nearest_neighbors import KNearestNeighbors
