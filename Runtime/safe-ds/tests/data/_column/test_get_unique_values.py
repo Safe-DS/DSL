4c3e7f3e338a8e55b3e7f061bedbb54dd55967b4
GideonKoenig <44088734+GideonKoenig@users.noreply.github.com>
2023-01-13 15:51:24 +0100

    Update Runtime/safe-ds/tests/data/_column/test_get_unique_values.py
    Co-authored-by: WinPlay02 <winplay02_gh@woberlaender.de>

--- Runtime/safe-ds/tests/data/_column/test_get_unique_values.py
+++ Runtime/safe-ds/tests/data/_column/test_get_unique_values.py
@@ -12,6 +12,6 @@ def test_get_unique_values(
     values: list[typing.Any], unique_values: list[typing.Any]
 ) -> None:
     column: Column = Column(values, "")
-    extracted_unique_values: list[typing.Any] = column.get_unique_value()
+    extracted_unique_values: list[typing.Any] = column.get_unique_values()
 
     assert extracted_unique_values == unique_values
