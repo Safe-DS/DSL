370a9e3843a18d71e06ae234de4b4ad4ae4b6ef5
Jonas B <97200640+SmiteDeluxe@users.noreply.github.com>
2023-01-20 16:21:07 +0100

    Update Runtime/safe-ds/safe_ds/plotting/_plot_lineplot.py
    Co-authored-by: WinPlay02 <winplay02_gh@woberlaender.de>

--- Runtime/safe-ds/safe_ds/plotting/_plot_lineplot.py
+++ Runtime/safe-ds/safe_ds/plotting/_plot_lineplot.py
@@ -13,7 +13,7 @@ def plot_lineplot(table: Table, x: str, y: str) -> None:
     table: Table
         The table containing the data to plot.
     x: str
-        The column name of the column that is to be plotted on the x-Axis.
+        The column name of the column to be plotted on the x-Axis.
     y str
         The column name of the column to be plotted on the y-Axis.
     """
