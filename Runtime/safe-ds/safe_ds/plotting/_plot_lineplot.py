e032e750b48fc5fe87493a89a5deff5296691908
SmiteDeluxe <smitedeluxe@gmail.com>
2023-01-20 16:13:16 +0100

    unused imports

--- Runtime/safe-ds/safe_ds/plotting/_plot_lineplot.py
+++ Runtime/safe-ds/safe_ds/plotting/_plot_lineplot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from safe_ds.data import Column, Table
+from safe_ds.data import Table
 from safe_ds.exceptions import UnknownColumnNameError
 
 
