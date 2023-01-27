bf2d59d0d2f3cc454acb77dd60bd1412a5b6a11e
Lars Reimann <mail@larsreimann.com>
2023-01-27 11:40:50 +0100

    docs: replace `sdsflow` with `sdspipe`

--- DSL/com.larsreimann.safeds/src/main/kotlin/com/larsreimann/safeds/emf/OriginalFilePath.kt
+++ DSL/com.larsreimann.safeds/src/main/kotlin/com/larsreimann/safeds/emf/OriginalFilePath.kt
@@ -3,6 +3,6 @@ package com.larsreimann.safeds.emf
 import org.eclipse.emf.common.notify.impl.AdapterImpl
 
 /**
- * Stores the original file path. This is necessary since synthetic resources always have the `sdsflow` extension.
+ * Stores the original file path. This is necessary since synthetic resources always have the `sdspipe` extension.
  */
 class OriginalFilePath(val path: String) : AdapterImpl()
