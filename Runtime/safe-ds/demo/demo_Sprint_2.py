ef5777e6b179d95332936f52d851355f20d0bd10
Lars Reimann <mail@larsreimann.com>
2023-01-28 15:05:51 +0100

    fix: demo

--- Runtime/safe-ds/demo/demo_Sprint_2.py
+++ Runtime/safe-ds/demo/demo_Sprint_2.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+
+from safeds.data import SupervisedDataset
 from safeds import plotting
-from safeds.data import SupervisedDataset, Table
-from safeds.regression import LinearRegression
+from safeds.data.tabular import Table
+from safeds.ml.regression import LinearRegression
 from sklearn.linear_model import LinearRegression as PythonLRS
 
 
