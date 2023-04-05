b203ec7290bec9ddba7dcc407ea1e29947dfe2c3
Lars Reimann <mail@larsreimann.com>
2023-04-05 20:13:10 +0200

    chore: add files created by the langium project generator

---
+++ DSL-langium/src/cli/generator.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import { CompositeGeneratorNode, toString } from 'langium';
+import path from 'path';
+import { SdsModule } from '../language-server/generated/ast';
+import { extractDestinationAndName } from './cli-util';
+
+export function generatePython(module: SdsModule, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.py`;
+
+    const fileNode = new CompositeGeneratorNode();
+    // fileNode.append('"use strict";', NL, NL);
+    // model.greetings.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.person.ref?.name}!');`, NL));
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
