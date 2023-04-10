82541a539206486fb08afb18412af4b2868773ea
Lars Reimann <mail@larsreimann.com>
2023-04-10 20:01:24 +0200

    test: more grammar tests for modules

--- DSL-langium/vitest.config.ts
+++ DSL-langium/vitest.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        // TODO: enable when vitest 0.30.0 is released
-        // chaiConfig: {
-        //     truncateThreshold: 0,
-        // },
+        chaiConfig: {
+            truncateThreshold: 0,
+        },
     },
 });
