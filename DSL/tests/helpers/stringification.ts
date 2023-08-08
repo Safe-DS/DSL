dd80c2fc58fa3d7f8103ea0a343868a372147c6c
Lars Reimann <mail@larsreimann.com>
2023-08-08 11:43:55 +0200

    test: stringify locations

--- DSL/tests/helpers/stringification.ts
+++ DSL/tests/helpers/stringification.ts
@@ -1,9 +1,22 @@
-import {Position, Range} from "vscode-languageserver";
+import { Location, Position, Range } from 'vscode-languageserver';
 
+/**
+ * Converts a position to a string.
+ */
 export const positionToString = (position: Position): string => {
     return `${position.line}:${position.character}`;
-}
+};
 
+/**
+ * Converts a range to a string.
+ */
 export const rangeToString = (range: Range): string => {
     return `${positionToString(range.start)}->${positionToString(range.end)}`;
+}
+
+/**
+ * Converts a location to a string.
+ */
+export const locationToString = (location: Location) => {
+    return `[${location.uri}] at ${rangeToString(location.range)}`;
 }
