83d0ac9228bfa3a2df4118f8183c01fb6b37e503
Lars Reimann <mail@larsreimann.com>
2023-08-10 23:24:12 +0200

    refactor: remove the `Runner` component (#547)
    ### Summary of Changes
    
    * Remove the `Runner` component. It's now in its own repository:
    https://github.com/Safe-DS/Runner
    * Pull the contents of the `DSL` directory up one level.
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

--- Runner/safeds_runner/codegen/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Declarations that may be referenced directly in the Python code that is generated for Safe-DS programs."""
-
-from ._operators import eager_and, eager_elvis, eager_or, safe_access
-
-__all__ = ["eager_and", "eager_elvis", "eager_or", "safe_access"]
