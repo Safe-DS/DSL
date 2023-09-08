9b303f3fc1f226a5036f4464009ed1b2f092b10e
Lars Reimann <mail@larsreimann.com>
2023-09-08 12:05:10 +0200

    test: initialize workspace so builtins are available

--- tests/language/builtins/workspaceManager.test.ts
+++ tests/language/builtins/workspaceManager.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
 import { listBuiltinsFiles } from '../../../src/language/builtins/workspaceManager.js';
 import { createSafeDsServices } from '../../../src/language/safe-ds-module.js';
 import { getLinkingErrors } from '../../helpers/diagnostics.js';
@@ -7,17 +7,24 @@ import { NodeFileSystem } from 'langium/node';
 const services = createSafeDsServices(NodeFileSystem).SafeDs;
 
 describe('SafeDsWorkspaceManager', () => {
+    beforeAll(async () => {
+        await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+    });
+
     describe('loadAdditionalDocuments', () => {
         it.each(['Any', 'Boolean', 'Float', 'Int', 'Nothing', 'Number', 'String'])(
             'should be possible to refer to %s',
             async (type) => {
-                const diagnostics = await getLinkingErrors(services, `
+                const diagnostics = await getLinkingErrors(
+                    services,
+                    `
 package test
 
 class C {
     attr a: ${type}
 }
-                `);
+                `,
+                );
                 expect(diagnostics).toHaveLength(0);
             },
         );
