74bf2e79a488aa636e78befcafcc0a26c0fd0636
Lars Reimann <mail@larsreimann.com>
2023-09-18 17:02:04 +0200

    feat: scoping of type parameter constraints

--- src/language/scoping/safe-ds-scope-computation.ts
+++ src/language/scoping/safe-ds-scope-computation.ts
@@ -1,3 +1,66 @@
-import { DefaultScopeComputation } from 'langium';
+import {
+    AstNode,
+    AstNodeDescription,
+    DefaultScopeComputation,
+    getContainerOfType,
+    LangiumDocument,
+    PrecomputedScopes,
+} from 'langium';
+import {
+    isSdsClass,
+    isSdsEnumVariant,
+    isSdsFunction,
+    isSdsTypeParameter,
+    isSdsTypeParameterList,
+    SdsTypeParameter,
+} from '../generated/ast.js';
 
-export class SafeDsScopeComputation extends DefaultScopeComputation {}
+export class SafeDsScopeComputation extends DefaultScopeComputation {
+    override processNode(node: AstNode, document: LangiumDocument, scopes: PrecomputedScopes): void {
+        if (isSdsTypeParameter(node)) {
+            this.processSdsTypeParameter(node, document, scopes);
+        } else {
+            super.processNode(node, document, scopes);
+        }
+    }
+
+    private processSdsTypeParameter(
+        node: SdsTypeParameter,
+        document: LangiumDocument,
+        scopes: PrecomputedScopes,
+    ): void {
+        const containingDeclaration = getContainerOfType(node, isSdsTypeParameterList)?.$container;
+        if (!containingDeclaration) {
+            return;
+        }
+
+        const name = this.nameProvider.getName(node);
+        const description = this.descriptions.createDescription(node, name, document);
+
+        if (isSdsClass(containingDeclaration)) {
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.parameterList, description);
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.constraintList, description);
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.body, description);
+        } else if (isSdsEnumVariant(containingDeclaration)) {
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.parameterList, description);
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.constraintList, description);
+        } else if (isSdsFunction(containingDeclaration)) {
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.parameterList, description);
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.resultList, description);
+            this.addToScopesIfKeyIsDefined(scopes, containingDeclaration.constraintList, description);
+        }
+    }
+
+    /**
+     * Adds the key/value pair to the scopes if the key is defined.
+     *
+     * @param scopes The scopes to add the key/value pair to.
+     * @param key The key.
+     * @param value The value.
+     */
+    addToScopesIfKeyIsDefined(scopes: PrecomputedScopes, key: AstNode | undefined, value: AstNodeDescription): void {
+        if (key) {
+            scopes.add(key, value);
+        }
+    }
+}
--- src/language/scoping/safe-ds-scope-provider.ts
+++ src/language/scoping/safe-ds-scope-provider.ts
@@ -5,13 +5,13 @@ import { resultsOrEmpty } from '../helpers/astShortcuts.js';
 export class SafeDsScopeProvider extends DefaultScopeProvider {
     override getScope(context: ReferenceInfo): Scope {
         if (isSdsYield(context.container) && context.property === 'result') {
-            return this.getScopeForYieldedResult(context);
+            return this.getScopeForYieldResult(context);
         } else {
             return super.getScope(context);
         }
     }
 
-    getScopeForYieldedResult(context: ReferenceInfo): Scope {
+    getScopeForYieldResult(context: ReferenceInfo): Scope {
         const containingSegment = getContainerOfType(context.container, isSdsSegment);
         if (!containingSegment) {
             return EMPTY_SCOPE;
