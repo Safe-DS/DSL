af13e28c4072e212ad1eb81910f7f1528ad6804f
Lars Reimann <mail@larsreimann.com>
2023-09-21 12:02:28 +0200

    feat: show info that empty constraint lists can be removed (#572)
    Closes #570
    
    ### Summary of Changes
    
    Show an info if a constraint list is empty.
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

--- src/language/validation/style.ts
+++ src/language/validation/style.ts
@@ -2,11 +2,13 @@ import {
     isSdsWildcard,
     SdsAnnotation,
     SdsAssignment,
-    SdsClass,
-    SdsEnum,
+    SdsClassBody,
+    SdsConstraintList,
+    SdsEnumBody,
     SdsEnumVariant,
     SdsFunction,
     SdsSegment,
+    SdsTypeParameterList,
     SdsUnionType,
 } from '../generated/ast.js';
 import { ValidationAcceptor } from 'langium';
@@ -15,6 +17,7 @@ import { isEmpty } from 'radash';
 export const CODE_STYLE_UNNECESSARY_ASSIGNMENT = 'style/unnecessary-assignment';
 export const CODE_STYLE_UNNECESSARY_ARGUMENT_LIST = 'style/unnecessary-argument-list';
 export const CODE_STYLE_UNNECESSARY_BODY = 'style/unnecessary-body';
+export const CODE_STYLE_UNNECESSARY_CONSTRAINT_LIST = 'style/unnecessary-constraint-list';
 export const CODE_STYLE_UNNECESSARY_ELVIS_OPERATOR = 'style/unnecessary-elvis-operator';
 export const CODE_STYLE_UNNECESSARY_SAFE_ACCESS = 'style/unnecessary-safe-access';
 export const CODE_STYLE_UNNECESSARY_PARAMETER_LIST = 'style/unnecessary-parameter-list';
@@ -44,22 +47,33 @@ export const assignmentShouldHaveMoreThanWildcardsAsAssignees = (
 // Unnecessary bodies
 // -----------------------------------------------------------------------------
 
-export const classBodyShouldNotBeEmpty = (node: SdsClass, accept: ValidationAcceptor) => {
-    if (node.body && isEmpty(node.body.members)) {
+export const classBodyShouldNotBeEmpty = (node: SdsClassBody, accept: ValidationAcceptor) => {
+    if (isEmpty(node.members)) {
         accept('info', 'This body can be removed.', {
             node,
-            property: 'body',
             code: CODE_STYLE_UNNECESSARY_BODY,
         });
     }
 };
 
-export const enumBodyShouldNotBeEmpty = (node: SdsEnum, accept: ValidationAcceptor) => {
-    if (node.body && isEmpty(node.body.variants)) {
+export const enumBodyShouldNotBeEmpty = (node: SdsEnumBody, accept: ValidationAcceptor) => {
+    if (isEmpty(node.variants)) {
         accept('info', 'This body can be removed.', {
             node,
-            property: 'body',
             code: CODE_STYLE_UNNECESSARY_BODY,
+        });
+    }
+};
+
+// -----------------------------------------------------------------------------
+// Unnecessary constraint list
+// -----------------------------------------------------------------------------
+
+export const constraintListShouldNotBeEmpty = (node: SdsConstraintList, accept: ValidationAcceptor) => {
+    if (isEmpty(node.constraints)) {
+        accept('info', 'This constraint list can be removed.', {
+            node,
+            code: CODE_STYLE_UNNECESSARY_CONSTRAINT_LIST,
         });
     }
 };
@@ -116,31 +130,10 @@ export const segmentResultListShouldNotBeEmpty = (node: SdsSegment, accept: Vali
 // Unnecessary type parameter lists
 // -----------------------------------------------------------------------------
 
-export const classTypeParameterListShouldNotBeEmpty = (node: SdsClass, accept: ValidationAcceptor) => {
-    if (node.typeParameterList && isEmpty(node.typeParameterList.typeParameters)) {
+export const typeParameterListShouldNotBeEmpty = (node: SdsTypeParameterList, accept: ValidationAcceptor) => {
+    if (isEmpty(node.typeParameters)) {
         accept('info', 'This type parameter list can be removed.', {
             node,
-            property: 'typeParameterList',
-            code: CODE_STYLE_UNNECESSARY_TYPE_PARAMETER_LIST,
-        });
-    }
-};
-
-export const enumVariantTypeParameterListShouldNotBeEmpty = (node: SdsEnumVariant, accept: ValidationAcceptor) => {
-    if (node.typeParameterList && isEmpty(node.typeParameterList.typeParameters)) {
-        accept('info', 'This type parameter list can be removed.', {
-            node,
-            property: 'typeParameterList',
-            code: CODE_STYLE_UNNECESSARY_TYPE_PARAMETER_LIST,
-        });
-    }
-};
-
-export const functionTypeParameterListShouldNotBeEmpty = (node: SdsFunction, accept: ValidationAcceptor) => {
-    if (node.typeParameterList && isEmpty(node.typeParameterList.typeParameters)) {
-        accept('info', 'This type parameter list can be removed.', {
-            node,
-            property: 'typeParameterList',
             code: CODE_STYLE_UNNECESSARY_TYPE_PARAMETER_LIST,
         });
     }
