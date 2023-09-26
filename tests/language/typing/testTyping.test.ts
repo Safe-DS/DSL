e6d1409c963087af054964b76501917c2b67c9d9
Lars Reimann <mail@larsreimann.com>
2023-09-26 11:21:41 +0200

    refactor: extract function to get node by location to test helpers

--- tests/language/typing/testTyping.test.ts
+++ tests/language/typing/testTyping.test.ts
@@ -2,13 +2,10 @@ import { afterEach, beforeEach, describe, it } from 'vitest';
 import { createSafeDsServices } from '../../../src/language/safe-ds-module.js';
 import { URI } from 'vscode-uri';
 import { NodeFileSystem } from 'langium/node';
-import { clearDocuments, isRangeEqual } from 'langium/test';
+import { clearDocuments } from 'langium/test';
 import { AssertionError } from 'assert';
-import { locationToString } from '../../helpers/location.js';
+import { getNodeByLocation, locationToString } from '../../helpers/location.js';
 import { createTypingTests } from './creator.js';
-import { AstNode, streamAllContents } from 'langium';
-import { Location, Range } from 'vscode-languageserver';
-import { isSdsModule } from '../../../src/language/generated/ast.js';
 import { computeType } from '../../../src/language/typing/typeComputer.js';
 
 const services = createSafeDsServices(NodeFileSystem).SafeDs;
@@ -39,11 +36,11 @@ describe('typing', async () => {
         for (const equivalenceClassAssertion of test.equivalenceClassAssertions) {
             if (equivalenceClassAssertion.locations.length > 1) {
                 const firstLocation = equivalenceClassAssertion.locations[0];
-                const firstNode = getNodeByLocation(firstLocation);
+                const firstNode = getNodeByLocation(services, firstLocation);
                 const firstType = computeType(firstNode);
 
                 for (const currentLocation of equivalenceClassAssertion.locations.slice(1)) {
-                    const currentNode = getNodeByLocation(currentLocation);
+                    const currentNode = getNodeByLocation(services, currentLocation);
                     const currentType = computeType(currentNode);
 
                     if (!currentType.equals(firstType)) {
@@ -61,7 +58,7 @@ describe('typing', async () => {
 
         // Ensure the serialized type of the node matches the expected type
         for (const serializationAssertion of test.serializationAssertions) {
-            const node = getNodeByLocation(serializationAssertion.location);
+            const node = getNodeByLocation(services, serializationAssertion.location);
             const actualType = computeType(node);
 
             if (actualType.toString() !== serializationAssertion.expectedType) {
@@ -76,42 +73,3 @@ describe('typing', async () => {
         }
     });
 });
-
-/**
- * Find the AstNode at the given location. It must fill the range exactly.
- *
- * @param location The location of the node to find.
- * @returns The node at the given location.
- * @throws AssertionError If no matching node was found.
- */
-const getNodeByLocation = (location: Location): AstNode => {
-    const document = services.shared.workspace.LangiumDocuments.getOrCreateDocument(URI.parse(location.uri));
-    const root = document.parseResult.value;
-
-    if (!isSdsModule(root)) {
-        throw new AssertionError({
-            message: `The root node of ${location.uri} is not a SdsModule`,
-        });
-    }
-
-    for (const node of streamAllContents(root)) {
-        const actualRange = node.$cstNode?.range;
-        if (actualRange && isRangeEqual(actualRange, location.range)) {
-            return node;
-        }
-
-        const actualNameRange = getNameRange(node);
-        if (actualNameRange && isRangeEqual(actualNameRange, location.range)) {
-            return node;
-        }
-    }
-
-    throw new AssertionError({ message: `Expected to find a node at ${locationToString(location)} but found none.` });
-};
-
-/**
- * Returns the range of the name of the given node or undefined if the node has no name.
- */
-const getNameRange = (node: AstNode): Range | undefined => {
-    return services.references.NameProvider.getNameNode(node)?.range;
-};
