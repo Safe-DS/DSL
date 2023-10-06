12a7cdff98f5e3d1c839a8f4785f2a608dd23e7a
Lars Reimann <mail@larsreimann.com>
2023-10-06 10:53:26 +0200

    feat: IdManager

---
+++ src/language/helpers/idManager.ts
@@ -0,0 +1,34 @@
+/**
+ * Handles the mapping of objects, usually nodes of an Safe-DS AST, to their IDs.
+ */
+export class IdManager<T extends WeakKey> {
+    /**
+     * Maps an object to an ID.
+     */
+    private objToId: WeakMap<T, Id> = new WeakMap();
+
+    /**
+     * The next available ID.
+     */
+    private nextId = 0;
+
+    /**
+     * Assigns the next available ID to the given object unless it already has one and returns the ID for this object.
+     */
+    assignId(obj: T): Id {
+        if (!this.objToId.has(obj)) {
+            this.objToId.set(obj, this.nextId++);
+        }
+        return this.objToId.get(obj)!;
+    }
+
+    /**
+     * Removes all mappings between object and ID and resets the counter.
+     */
+    reset() {
+        this.objToId = new WeakMap();
+        this.nextId = 0;
+    }
+}
+
+export type Id = number;
