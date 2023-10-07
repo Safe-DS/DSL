527c45ff8bd9399dcb77c88e9fd9cde6ae7d0aa7
Lars Reimann <mail@larsreimann.com>
2023-10-07 16:11:38 +0200

    refactor: use URIs within grammar tests

--- tests/language/grammar/creator.ts
+++ tests/language/grammar/creator.ts
@@ -1,43 +1,43 @@
-import { listSafeDSResources_PathBased, resolvePathRelativeToResources_PathBased } from '../../helpers/testResources.js';
-import path from 'path';
+import { listSafeDsFiles, uriToShortenedResourceName } from '../../helpers/testResources.js';
 import fs from 'fs';
 import { findTestComments } from '../../helpers/testComments.js';
 import { NoCommentsError } from '../../helpers/testChecks.js';
 import { TestDescription } from '../../helpers/testDescription.js';
+import { URI } from 'langium';
 
-const root = 'grammar';
+const rootResourceName = 'grammar';
 
 export const createGrammarTests = (): GrammarTest[] => {
-    return listSafeDSResources_PathBased(root).map(createGrammarTest);
+    return listSafeDsFiles(rootResourceName).map(createGrammarTest);
 };
 
-const createGrammarTest = (relativeResourcePath: string): GrammarTest => {
-    const absolutePath = resolvePathRelativeToResources_PathBased(path.join(root, relativeResourcePath));
-    const code = fs.readFileSync(absolutePath).toString();
+const createGrammarTest = (uri: URI): GrammarTest => {
+    const shortenedResourceName = uriToShortenedResourceName(uri, rootResourceName);
+    const code = fs.readFileSync(uri.fsPath).toString();
     const comments = findTestComments(code);
 
     // Must contain at least one comment
     if (comments.length === 0) {
-        return invalidTest(relativeResourcePath, new NoCommentsError());
+        return invalidTest(shortenedResourceName, new NoCommentsError());
     }
 
     // Must contain no more than one comment
     if (comments.length > 1) {
-        return invalidTest(relativeResourcePath, new MultipleCommentsError(comments));
+        return invalidTest(shortenedResourceName, new MultipleCommentsError(comments));
     }
 
     const comment = comments[0];
 
     // Must contain a valid comment
     if (comment !== 'syntax_error' && comment !== 'no_syntax_error') {
-        return invalidTest(relativeResourcePath, new InvalidCommentError(comment));
+        return invalidTest(shortenedResourceName, new InvalidCommentError(comment));
     }
 
     let testName: string;
     if (comment === 'syntax_error') {
-        testName = `[${relativeResourcePath}] should have syntax errors`;
+        testName = `[${shortenedResourceName}] should have syntax errors`;
     } else {
-        testName = `[${relativeResourcePath}] should not have syntax errors`;
+        testName = `[${shortenedResourceName}] should not have syntax errors`;
     }
 
     return {
