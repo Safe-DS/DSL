17a5b7aca33e6518d44996f693279608a6fe0ba4
Lars Reimann <mail@larsreimann.com>
2023-10-11 12:39:51 +0200

    feat: error if single use annotations are used multiple times (#631)
    Closes partially #543
    
    ### Summary of Changes
    
    Show an error if annotation that is not repeatable (default, unless it
    has the `@Repeatable` annotation) is repeated.
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

--- src/language/validation/safe-ds-validator.ts
+++ src/language/validation/safe-ds-validator.ts
@@ -76,6 +76,7 @@ import {
 } from './other/declarations/annotationCalls.js';
 import { memberAccessMustBeNullSafeIfReceiverIsNullable } from './other/expressions/memberAccesses.js';
 import { importPackageMustExist, importPackageShouldNotBeEmpty } from './other/imports.js';
+import { singleUseAnnotationsMustNotBeRepeated } from './builtins/repeatable.js';
 
 /**
  * Register custom validation checks.
@@ -98,6 +99,7 @@ export const registerValidationChecks = function (services: SafeDsServices) {
             annotationCallAnnotationShouldNotBeExperimental(services),
             annotationCallArgumentListShouldBeNeeded,
         ],
+        SdsAnnotatedObject: [singleUseAnnotationsMustNotBeRepeated(services)],
         SdsArgument: [
             argumentCorrespondingParameterShouldNotBeDeprecated(services),
             argumentCorrespondingParameterShouldNotBeExperimental(services),
