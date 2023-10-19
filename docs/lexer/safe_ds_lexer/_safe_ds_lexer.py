1f52173e5ff02cfd1fa4c78edad0d4f8580debbb
Lars Reimann <mail@larsreimann.com>
2023-10-20 01:09:21 +0200

    docs: improve syntax highlighting

--- docs/lexer/safe_ds_lexer/_safe_ds_lexer.py
+++ docs/lexer/safe_ds_lexer/_safe_ds_lexer.py
@@ -1,40 +1,48 @@
-
 from pygments.lexer import RegexLexer, words
 from pygments.token import Comment, Keyword, Name, Number, Operator, String, Whitespace
 
-constants = (
+keywords_constants = (
     "false",
     "null",
     "true",
 )
 
-keywords = (
+keywords_class = (
+    "class",
+    "enum",
+)
+
+keywords_declaration = (
+    "val",
+)
+
+keywords_function = (
+    "fun",
+    "pipeline",
+    "segment",
+)
+
+keywords_namespace = (
+    "from",
+    "package",
+)
+
+keywords_generic = (
     "annotation",
     "as",
     "attr",
-    "class",
     "const",
-    "enum",
-    "fun",
+    "import",
     "in",
     "internal",
     "literal",
     "out",
-    "pipeline",
     "private",
     "schema",
-    "segment",
     "static",
     "union",
-    "val",
     "where",
     "yield",
-)
-
-namespace = (
-    "from",
-    "import",
-    "package",
 )
 
 operators = (
@@ -44,6 +52,10 @@ operators = (
     "sub",
     "super",
 )
+
+
+identifier_regex = r"[_a-zA-Z][_a-zA-Z0-9]*|`[_a-zA-Z][_a-zA-Z0-9]*`"
+qualified_name_regex = fr"{identifier_regex}(\.{identifier_regex})*"
 
 
 class SafeDsLexer(RegexLexer):
@@ -60,16 +72,42 @@ class SafeDsLexer(RegexLexer):
 
     tokens = {
         "root": [
+            # Literals
             (r"\b([0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?)\b", Number),
             (r'"|}}', String, "string"),
-            (words(constants, prefix=r"\b", suffix=r"\b"), Keyword.Constant),
-            (words(keywords, prefix=r"\b", suffix=r"\b"), Keyword),
-            (words(namespace, prefix=r"\b", suffix=r"\b"), Keyword.Namespace),
+
+            # Keywords
+            (words(keywords_constants, prefix=r"\b", suffix=r"\b"), Keyword.Constant),
+            (words(keywords_declaration, prefix=r"\b", suffix=r"\b"), Keyword.Declaration, "constant"),
+            (words(keywords_class, prefix=r"\b", suffix=r"\b"), Keyword, "class"),
+            (words(keywords_function, prefix=r"\b", suffix=r"\b"), Keyword, "function"),
+            (words(keywords_namespace, prefix=r"\b", suffix=r"\b"), Keyword.Namespace, "namespace"),
+            (words(keywords_generic, prefix=r"\b", suffix=r"\b"), Keyword),
+
+            # Operators
             (words(operators, prefix=r"\b", suffix=r"\b"), Operator.Word),
-            (r"`[_a-zA-Z][_a-zA-Z0-9]*`", Name),
+
+            # Identifiers
+            (identifier_regex, Name),
+
+            # Comments
             (r"//.+?$", Comment.Single),
             (r"/\*[\s\S]*?\*/", Comment.Multiline),
+
+            # Whitespace
             (r"\s+", Whitespace),
+        ],
+        "class": [
+            (identifier_regex, Name.Class, "#pop"),
+        ],
+        "constant": [
+            (identifier_regex, Name.Constant, "#pop"),
+        ],
+        "function": [
+            (identifier_regex, Name.Function, "#pop"),
+        ],
+        "namespace": [
+            (qualified_name_regex, Name.Namespace, "#pop"),
         ],
         "string": [
             (r'([^"{]|\{(?!\{))+', String),
