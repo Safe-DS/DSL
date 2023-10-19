721cc04fa35478b10bee16bb9c84025d95e5e699
Lars Reimann <mail@larsreimann.com>
2023-10-19 22:44:31 +0200

    docs: finish syntax highlighting

--- docs/lexer/safe_ds_lexer/_safe_ds_lexer.py
+++ docs/lexer/safe_ds_lexer/_safe_ds_lexer.py
@@ -3,6 +3,7 @@
 from pygments.lexer import RegexLexer, words
 from pygments.token import *
 
+
 class SafeDsLexer(RegexLexer):
     name = 'safe-ds'
     aliases = [
@@ -15,46 +16,64 @@ class SafeDsLexer(RegexLexer):
     ]
     filenames = ['*.sdspipe', '*.sdsstub', '*.sdstest']
 
+    constants = (
+        'false',
+        'null',
+        'true'
+    )
+
     keywords = (
-        'and',
         'annotation',
         'as',
         'attr',
         'class',
         'const',
         'enum',
-        'false',
-        'from',
         'fun',
-        'import',
         'in',
         'internal',
         'literal',
-        'not',
-        'null',
-        'or',
         'out',
-        'package',
         'pipeline',
         'private',
         'schema',
         'segment',
         'static',
-        'sub',
-        'super',
-        'true',
         'union',
         'val',
         'where',
         'yield',
     )
 
+    namespace = (
+        'from',
+        'import',
+        'package',
+    )
+
+    operators = (
+        'and',
+        'not',
+        'or',
+        'sub',
+        'super'
+    )
+
     tokens = {
         'root': [
-            (words(keywords, suffix=r'\b'), Name.Builtin),
-            (r'[0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?', Number),
+            (r'\b([0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?)\b', Number),
+            (r'"|}}', String, 'string'),
+            (words(constants, prefix=r'\b', suffix=r'\b'), Keyword.Constant),
+            (words(keywords, prefix=r'\b', suffix=r'\b'), Keyword),
+            (words(namespace, prefix=r'\b', suffix=r'\b'), Keyword.Namespace),
+            (words(operators, prefix=r'\b', suffix=r'\b'), Operator.Word),
+            (r'`[_a-zA-Z][_a-zA-Z0-9]*`', Name),
             (r'//.+?$', Comment.Single),
             (r'/\*[\s\S]*?\*/', Comment.Multiline),
             (r'\s+', Whitespace),
+        ],
+        'string': [
+            (r'([^"{]|\{(?!\{))+', String),
+            (r'\{\{|"', String, '#pop'),
         ],
     }
