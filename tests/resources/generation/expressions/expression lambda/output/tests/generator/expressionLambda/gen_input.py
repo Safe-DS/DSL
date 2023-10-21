3554d22b59309ae61a5d61f35269d2d082d3cb43
Lars Reimann <mail@larsreimann.com>
2023-10-21 20:40:51 +0200

    test: remove unneeded code

--- tests/resources/generation/expressions/expression lambda/output/tests/generator/expressionLambda/gen_input.py
+++ tests/resources/generation/expressions/expression lambda/output/tests/generator/expressionLambda/gen_input.py
@@ -1,5 +1,5 @@
 # Pipelines --------------------------------------------------------------------
 
 def test():
-    f1(lambda a, b=2: 1)
-    f2(lambda a, c: 1)
+    f(lambda a, b=2: 1)
+    f(lambda a, c: 1)
