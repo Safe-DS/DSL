447bdc1c843c09a33bfe767ea8dfd3334ebe2849
Lars Reimann <mail@larsreimann.com>
2023-10-21 20:42:55 +0200

    test: remove unneeded code

--- tests/resources/generation/declarations/parameter with python name/output/tests/generator/parameterWithPythonName/gen_input.py
+++ tests/resources/generation/declarations/parameter with python name/output/tests/generator/parameterWithPythonName/gen_input.py
@@ -1,13 +1,7 @@
 # Steps ------------------------------------------------------------------------
 
-def test1(param1, param_2, param_3=0):
+def test(param1, param_2, param_3=0):
     f1(lambda param1, param2, param3=0: 1)
     def __block_lambda_0(param1, param2, param3=0):
-        pass
-    f2(__block_lambda_0)
-
-def test2(param1, param_2, param_4):
-    f1(lambda param1, param2, param4: 1)
-    def __block_lambda_0(param1, param2, param4):
         pass
     f2(__block_lambda_0)
