fc7d6aec4efdc0c9878541b932a59e1dc74de11d
Lars Reimann <mail@larsreimann.com>
2023-10-23 21:18:05 +0200

    feat: error if `@PythonName` and `@PythonCall` are set on a function

--- src/language/validation/builtins/pythonName.ts
+++ src/language/validation/builtins/pythonName.ts
@@ -1,9 +1,25 @@
 import { ValidationAcceptor } from 'langium';
-import { SdsDeclaration } from '../../generated/ast.js';
+import { SdsDeclaration, SdsFunction } from '../../generated/ast.js';
 import { SafeDsServices } from '../../safe-ds-module.js';
 import { findFirstAnnotationCallOf } from '../../helpers/nodeProperties.js';
 
+export const CODE_PYTHON_NAME_MUTUALLY_EXCLUSIVE_WITH_PYTHON_CALL = 'python-name/mutually-exclusive-with-python-call';
 export const CODE_PYTHON_NAME_SAME_AS_SAFE_DS_NAME = 'python-name/same-as-safe-ds-name';
+
+export const pythonNameMustNotBeSetIfPythonCallIsSet = (services: SafeDsServices) => {
+    const builtinAnnotations = services.builtins.Annotations;
+
+    return (node: SdsFunction, accept: ValidationAcceptor) => {
+        if (builtinAnnotations.getPythonCall(node)) {
+            const annotationCall = findFirstAnnotationCallOf(node, builtinAnnotations.PythonName)!;
+            accept('error', 'A Python name must not be set if a Python call is set.', {
+                node: annotationCall,
+                property: 'annotation',
+                code: CODE_PYTHON_NAME_MUTUALLY_EXCLUSIVE_WITH_PYTHON_CALL,
+            });
+        }
+    };
+};
 
 export const pythonNameShouldDifferFromSafeDsName = (services: SafeDsServices) => {
     const builtinAnnotations = services.builtins.Annotations;
