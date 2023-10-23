a11229360dd75f9dfb54ff7890d5da81245744db
Lars Reimann <mail@larsreimann.com>
2023-10-23 21:26:06 +0200

    fix: failing tests

--- src/language/validation/builtins/pythonName.ts
+++ src/language/validation/builtins/pythonName.ts
@@ -1,7 +1,7 @@
 import { ValidationAcceptor } from 'langium';
 import { SdsDeclaration, SdsFunction } from '../../generated/ast.js';
 import { SafeDsServices } from '../../safe-ds-module.js';
-import { findFirstAnnotationCallOf } from '../../helpers/nodeProperties.js';
+import { findFirstAnnotationCallOf, hasAnnotationCallOf } from '../../helpers/nodeProperties.js';
 
 export const CODE_PYTHON_NAME_MUTUALLY_EXCLUSIVE_WITH_PYTHON_CALL = 'python-name/mutually-exclusive-with-python-call';
 export const CODE_PYTHON_NAME_SAME_AS_SAFE_DS_NAME = 'python-name/same-as-safe-ds-name';
@@ -10,14 +10,20 @@ export const pythonNameMustNotBeSetIfPythonCallIsSet = (services: SafeDsServices
     const builtinAnnotations = services.builtins.Annotations;
 
     return (node: SdsFunction, accept: ValidationAcceptor) => {
-        if (builtinAnnotations.getPythonCall(node)) {
-            const annotationCall = findFirstAnnotationCallOf(node, builtinAnnotations.PythonName)!;
-            accept('error', 'A Python name must not be set if a Python call is set.', {
-                node: annotationCall,
-                property: 'annotation',
-                code: CODE_PYTHON_NAME_MUTUALLY_EXCLUSIVE_WITH_PYTHON_CALL,
-            });
+        if (!hasAnnotationCallOf(node, builtinAnnotations.PythonCall)) {
+            return;
         }
+
+        const firstPythonName = findFirstAnnotationCallOf(node, builtinAnnotations.PythonName);
+        if (!firstPythonName) {
+            return;
+        }
+
+        accept('error', 'A Python name must not be set if a Python call is set.', {
+            node: firstPythonName,
+            property: 'annotation',
+            code: CODE_PYTHON_NAME_MUTUALLY_EXCLUSIVE_WITH_PYTHON_CALL,
+        });
     };
 };
 
