4eea668464611a5b5c621f09838ac3af7b3ec97f
Lars Reimann <mail@larsreimann.com>
2023-10-24 14:09:35 +0200

    feat: comment provider

---
+++ src/language/documentation/safe-ds-comment-provider.ts
@@ -0,0 +1,33 @@
+import { AstNode, DefaultCommentProvider, isAstNodeWithComment } from 'langium';
+import {
+    isSdsBlockLambdaResult,
+    isSdsDeclaration,
+    isSdsParameter,
+    isSdsPlaceholder,
+    isSdsResult,
+    isSdsTypeParameter,
+} from '../generated/ast.js';
+
+export class SafeDsCommentProvider extends DefaultCommentProvider {
+    override getComment(node: AstNode): string | undefined {
+        /* c8 ignore start */ if (isAstNodeWithComment(node)) {
+            return node.$comment;
+        } /* c8 ignore stop */ else if (
+            !isSdsDeclaration(node) ||
+            isSdsBlockLambdaResult(node) ||
+            isSdsParameter(node) ||
+            isSdsPlaceholder(node) ||
+            isSdsResult(node) ||
+            isSdsTypeParameter(node)
+        ) {
+            return undefined;
+        }
+
+        // The annotation call list is the previous sibling of the declaration in the CST, so we must step past it
+        if (node.annotationCallList) {
+            return super.getComment(node.annotationCallList);
+        } else {
+            return super.getComment(node);
+        }
+    }
+}
