918011eaf7a37727dd1e5a5a9a975178000ae5e0
Lars Reimann <mail@larsreimann.com>
2023-10-26 16:39:06 +0200

    build: ignore files in root

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     plugins: [
         ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
         ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
-        ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+        ['@semantic-release/changelog', { changelogFile: 'packages/safe-ds-vscode/CHANGELOG.md' }],
         ['semantic-release-vsce', { packageVsix: true }],
         [
             '@semantic-release/github',
