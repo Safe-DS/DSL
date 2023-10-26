a15951da88f28c19cf1bb47dcb684b6ec074cfb1
Lars Reimann <mail@larsreimann.com>
2023-10-26 22:53:11 +0200

    refactor: remove references to `__dirname` where possible (#696)
    ### Summary of Changes
    
    Where possible, replace `__dirname` (which does not work in modules)
    with `meta.import.url`, which does. Once VS Code can handle ESM, we can
    set the output format to "ESM" in the ESBuild configs.
    
    See also:
    * [Node.js
    Docs](https://nodejs.org/dist/latest/docs/api/esm.html#esm_no_filename_or_dirname)
    *
    [StackOverflow](https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules)

--- packages/safe-ds-cli/tests/main.test.ts
+++ packages/safe-ds-cli/tests/main.test.ts
@@ -1,11 +1,7 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import path from 'node:path';
 import { execSync, spawnSync } from 'node:child_process';
-import url from 'node:url';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const projectRoot = path.resolve(__dirname, '..');
+const projectRoot = new URL('..', import.meta.url);
 
 describe('program', () => {
     beforeAll(() => {
