228f70de99f2368ac75b68410ac148043feb7d66
Lars Reimann <mail@larsreimann.com>
2023-11-23 10:03:13 +0100

    fix: wrong import path

--- packages/safe-ds-eda/globals.d.ts
+++ packages/safe-ds-eda/globals.d.ts
@@ -1,14 +1,13 @@
-import * as _vscode from "vscode";
-import type { State, ToExtensionMessage } from "../types/messaging";
+import type { ToExtensionMessage } from '../../types/shared-eda-vscode/messaging.js';
 
 declare global {
-  interface Window {
-    injVscode: {
-      postMessage: (message: ToExtensionMessage) => void;
-      // getState: () => State[];
-      // setState: (state: State[]) => void;
-    };
-    tableIdentifier: string;
-    pythonServerPort: number;
-  }
+    interface Window {
+        injVscode: {
+            postMessage: (message: ToExtensionMessage) => void;
+            // getState: () => State[];
+            // setState: (state: State[]) => void;
+        };
+        tableIdentifier: string;
+        pythonServerPort: number;
+    }
 }
