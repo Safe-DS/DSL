d8c0c151eb567f75ca9e0f2ad7d0731a357884ed
Lars Reimann <mail@larsreimann.com>
2023-11-24 10:23:03 +0100

    feat: error if list index is in bounds

--- packages/safe-ds-lang/src/language/validation/other/expressions/indexedAccess.ts
+++ packages/safe-ds-lang/src/language/validation/other/expressions/indexedAccess.ts
@@ -8,7 +8,6 @@ export const CODE_INDEXED_ACCESS_INVALID_INDEX = 'indexed-access/invalid-index';
 export const indexedAccessIndexMustBeValid = (services: SafeDsServices) => {
     const coreTypes = services.types.CoreTypes;
     const partialEvaluator = services.evaluation.PartialEvaluator;
-    const typeChecker = services.types.TypeChecker;
     const typeComputer = services.types.TypeComputer;
 
     return (node: SdsIndexedAccess, accept: ValidationAcceptor): void => {
@@ -18,13 +17,42 @@ export const indexedAccessIndexMustBeValid = (services: SafeDsServices) => {
         }
 
         const receiverValue = partialEvaluator.evaluate(node.receiver);
-        console.log(receiverValue.toString());
-        if (indexValue instanceof IntConstant && receiverValue instanceof EvaluatedList) {
-            if (receiverValue.size) {
-            }
-        } else if (receiverValue instanceof EvaluatedMap) {
+
+        // Check map key
+        if (receiverValue instanceof EvaluatedMap) {
             if (!receiverValue.has(indexValue)) {
-                accept('error', `Map key '${indexValue}' does not exist.`, { node, property: 'index' });
+                accept('error', `Map key '${indexValue}' does not exist.`, {
+                    node,
+                    property: 'index',
+                    code: CODE_INDEXED_ACCESS_INVALID_INDEX,
+                });
+            }
+            return;
+        }
+
+        // Check list index
+        if (!(indexValue instanceof IntConstant)) {
+            return;
+        }
+
+        if (receiverValue instanceof EvaluatedList) {
+            if (indexValue.value < 0 || indexValue.value >= receiverValue.size) {
+                accept('error', `List index '${indexValue}' is out of bounds.`, {
+                    node,
+                    property: 'index',
+                    code: CODE_INDEXED_ACCESS_INVALID_INDEX,
+                });
+            }
+        }
+
+        const receiverType = typeComputer.computeType(node.receiver);
+        if (receiverType.equals(coreTypes.List)) {
+            if (indexValue.value < 0) {
+                accept('error', `List index '${indexValue}' is out of bounds.`, {
+                    node,
+                    property: 'index',
+                    code: CODE_INDEXED_ACCESS_INVALID_INDEX,
+                });
             }
         }
     };
