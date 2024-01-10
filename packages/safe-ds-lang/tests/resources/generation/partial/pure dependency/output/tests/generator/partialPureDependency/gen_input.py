c85f8667c61603500df59da625fcb0b8732006e9
WinPlay02 <winplay02_gh@woberlaender.de>
2024-01-10 16:21:02 +0100

    test: split partial execution tests into smaller tests

--- packages/safe-ds-lang/tests/resources/generation/partial/pure dependency/output/tests/generator/partialPureDependency/gen_input.py
+++ packages/safe-ds-lang/tests/resources/generation/partial/pure dependency/output/tests/generator/partialPureDependency/gen_input.py
@@ -46,25 +46,5 @@ def testPipeline():
     safeds_runner.server.pipeline_manager.runner_save_placeholder('listValue', listValue)
     listResult = g3(list)
     safeds_runner.server.pipeline_manager.runner_save_placeholder('listResult', listResult)
-    impureFileWrite = iFileWrite()
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('impureFileWrite', impureFileWrite)
-    impureFileWrite2 = iFileWrite()
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('impureFileWrite2', impureFileWrite2)
-    impureFileReadAgain = iFileRead()
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('impureFileReadAgain', impureFileReadAgain)
-    pureValueForImpure2 = noPartialEvalInt(2)
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('pureValueForImpure2', pureValueForImpure2)
-    pureValueForImpure3 = 3
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('pureValueForImpure3', pureValueForImpure3)
-    def __gen_block_lambda_1():
-        i1(1)
-        __gen_block_lambda_result_r = 1
-        return __gen_block_lambda_result_r
-    fp(__gen_block_lambda_1)
-    i1(1)
-    impureA1 = i1(pureValueForImpure2)
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('impureA1', impureA1)
-    impureA2 = i1(noPartialEvalInt(3))
-    safeds_runner.server.pipeline_manager.runner_save_placeholder('impureA2', impureA2)
-    result = (((((-(o)) + (1)) + (value1)) + (((mapResult) * (listResult)) / (g4(listValue)))) - (impureA2)) + (impureFileReadAgain)
+    result = (((-(o)) + (1)) + (value1)) + (((mapResult) * (listResult)) / (g4(listValue)))
     safeds_runner.server.pipeline_manager.runner_save_placeholder('result', result)
