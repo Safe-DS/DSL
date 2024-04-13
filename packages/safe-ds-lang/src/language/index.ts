b33b995010197f3d26189ca9849933cf3a1623a1
Lars Reimann <mail@larsreimann.com>
2024-04-13 20:16:35 +0200

    feat: updater for the runner

--- packages/safe-ds-lang/src/language/index.ts
+++ packages/safe-ds-lang/src/language/index.ts
@@ -1,4 +1,10 @@
-import { pipVersionRange, RPC_RUNNER_INSTALL, RPC_RUNNER_START, RPC_RUNNER_STARTED } from './runner/safe-ds-runner.js';
+import {
+    pipVersionRange,
+    RPC_RUNNER_INSTALL,
+    RPC_RUNNER_START,
+    RPC_RUNNER_STARTED,
+    RPC_RUNNER_UPDATE,
+} from './runner/safe-ds-runner.js';
 
 // Services
 export type { SafeDsServices } from './safe-ds-module.js';
@@ -25,6 +31,7 @@ export const rpc = {
     runnerInstall: RPC_RUNNER_INSTALL,
     runnerStart: RPC_RUNNER_START,
     runnerStarted: RPC_RUNNER_STARTED,
+    runnerUpdate: RPC_RUNNER_UPDATE,
 };
 
 // Dependencies
