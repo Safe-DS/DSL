6cbc4f3fed0166d00e2d3ffd2c62b2e8ddfafc46
Lars Reimann <mail@larsreimann.com>
2024-04-27 09:23:43 +0200

    test: update codegen test

--- packages/safe-ds-lang/tests/resources/generation/python/runner integration/expressions/this/generated/tests/generator/runnerIntegration/expressions/this/gen_input.py
+++ packages/safe-ds-lang/tests/resources/generation/python/runner integration/expressions/this/generated/tests/generator/runnerIntegration/expressions/this/gen_input.py
@@ -6,23 +6,18 @@ from tests.generator.runnerIntegration.expressions.this import MyClass
 # Pipelines --------------------------------------------------------------------
 
 def myPipeline():
+    __gen_receiver_0 = safeds_runner.memoized_static_call(
+        "tests.generator.runnerIntegration.expressions.this.MyClass",
+        MyClass,
+        [],
+        {},
+        []
+    )
     a = safeds_runner.memoized_dynamic_call(
-        safeds_runner.memoized_static_call(
-            "tests.generator.runnerIntegration.expressions.this.MyClass",
-            MyClass,
-            [],
-            {},
-            []
-        ),
+        __gen_receiver_0,
         "myFunction",
         [],
-        {"p": safeds_runner.memoized_static_call(
-            "tests.generator.runnerIntegration.expressions.this.MyClass",
-            MyClass,
-            [],
-            {},
-            []
-        )},
+        {"p": __gen_receiver_0},
         []
     )
     safeds_runner.save_placeholder('a', a)
