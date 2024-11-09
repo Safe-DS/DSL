cc945c6eddc230e4498b5b2da1374dac447015a1
Lars Reimann <mail@larsreimann.com>
2024-11-09 15:27:41 +0100

    feat: update code lens provider

--- packages/safe-ds-lang/src/language/lsp/safe-ds-code-lens-provider.ts
+++ packages/safe-ds-lang/src/language/lsp/safe-ds-code-lens-provider.ts
@@ -1,23 +1,46 @@
 import { CodeLensProvider } from 'langium/lsp';
-import { CancellationToken, CodeLens, type CodeLensParams } from 'vscode-languageserver';
+import { CancellationToken, CodeLens, type CodeLensParams, Range } from 'vscode-languageserver';
 import { SafeDsServices } from '../safe-ds-module.js';
 import { SafeDsTypeComputer } from '../typing/safe-ds-type-computer.js';
 import { AstNode, AstNodeLocator, AstUtils, interruptAndCheck, LangiumDocument } from 'langium';
-import { isSdsModule, isSdsPipeline, SdsModuleMember, SdsPipeline, SdsPlaceholder } from '../generated/ast.js';
+import {
+    isSdsAssignment,
+    isSdsCall,
+    isSdsClass,
+    isSdsMemberAccess,
+    isSdsModule,
+    isSdsOutputStatement,
+    isSdsPipeline,
+    isSdsPlaceholder,
+    SdsAssignment,
+    SdsModuleMember,
+    SdsOutputStatement,
+    SdsPipeline,
+    SdsPlaceholder,
+} from '../generated/ast.js';
 import { SafeDsRunner } from '../runtime/safe-ds-runner.js';
-import { getModuleMembers, streamPlaceholders } from '../helpers/nodeProperties.js';
+import { getAbstractResults, getAssignees, getModuleMembers, getStatements } from '../helpers/nodeProperties.js';
 import { SafeDsTypeChecker } from '../typing/safe-ds-type-checker.js';
 
-import { COMMAND_PRINT_VALUE, COMMAND_RUN_PIPELINE, COMMAND_SHOW_IMAGE } from '../communication/commands.js';
+import {
+    COMMAND_EXPLORE_TABLE,
+    COMMAND_PRINT_VALUE,
+    COMMAND_RUN_PIPELINE,
+    COMMAND_SHOW_IMAGE,
+} from '../communication/commands.js';
+import { SafeDsNodeMapper } from '../helpers/safe-ds-node-mapper.js';
+import { NamedTupleType, Type } from '../typing/model.js';
 
 export class SafeDsCodeLensProvider implements CodeLensProvider {
     private readonly astNodeLocator: AstNodeLocator;
+    private readonly nodeMapper: SafeDsNodeMapper;
     private readonly runner: SafeDsRunner;
     private readonly typeChecker: SafeDsTypeChecker;
     private readonly typeComputer: SafeDsTypeComputer;
 
     constructor(services: SafeDsServices) {
         this.astNodeLocator = services.workspace.AstNodeLocator;
+        this.nodeMapper = services.helpers.NodeMapper;
         this.runner = services.runtime.Runner;
         this.typeChecker = services.typing.TypeChecker;
         this.typeComputer = services.typing.TypeComputer;
@@ -57,9 +80,13 @@ export class SafeDsCodeLensProvider implements CodeLensProvider {
         if (isSdsPipeline(node)) {
             await this.computeCodeLensForPipeline(node, accept);
 
-            for (const placeholder of streamPlaceholders(node.body)) {
+            for (const statement of getStatements(node.body)) {
                 await interruptAndCheck(cancelToken);
-                await this.computeCodeLensForPlaceholder(placeholder, accept);
+                if (isSdsAssignment(statement)) {
+                    await this.computeCodeLensForAssignment(statement, accept);
+                } else if (isSdsOutputStatement(statement)) {
+                    await this.computeCodeLensForOutputStatement(statement, accept);
+                }
             }
         }
     }
@@ -81,6 +108,19 @@ export class SafeDsCodeLensProvider implements CodeLensProvider {
         });
     }
 
+    private async computeCodeLensForAssignment(
+        node: SdsAssignment,
+        accept: CodeLensAcceptor,
+        cancelToken: CancellationToken = CancellationToken.None,
+    ): Promise<void> {
+        for (const assignee of getAssignees(node)) {
+            await interruptAndCheck(cancelToken);
+            if (isSdsPlaceholder(assignee)) {
+                await this.computeCodeLensForPlaceholder(assignee, accept);
+            }
+        }
+    }
+
     private async computeCodeLensForPlaceholder(node: SdsPlaceholder, accept: CodeLensAcceptor): Promise<void> {
         const cstNode = node.$cstNode;
         if (!cstNode) {
@@ -88,40 +128,99 @@ export class SafeDsCodeLensProvider implements CodeLensProvider {
             return;
         }
 
-        if (this.typeChecker.isImage(this.typeComputer.computeType(node))) {
-            const documentUri = AstUtils.getDocument(node).uri.toString();
-            const nodePath = this.astNodeLocator.getAstNodePath(node);
+        const type = this.typeComputer.computeType(node);
+        await this.computeCodeLensForValue(type, node.name, this.computeNodeId(node), cstNode.range, accept);
+    }
 
+    private async computeCodeLensForOutputStatement(
+        node: SdsOutputStatement,
+        accept: CodeLensAcceptor,
+        cancelToken: CancellationToken = CancellationToken.None,
+    ): Promise<void> {
+        const cstNode = node.$cstNode;
+        if (!cstNode) {
+            /* c8 ignore next 2 */
+            return;
+        }
+
+        // Compute type of expression and unpack if it is a named tuple
+        const expressionType = this.typeComputer.computeType(node.expression);
+        let unpackedTypes: Type[] = [expressionType];
+        if (expressionType instanceof NamedTupleType) {
+            unpackedTypes = expressionType.entries.map((it) => it.type);
+        }
+
+        // Get names of values
+        let valueNames: string[] = [];
+        if (isSdsCall(node.expression)) {
+            const callable = this.nodeMapper.callToCallable(node.expression);
+            if (isSdsClass(callable)) {
+                valueNames = [`${callable.name} instance`];
+            } else {
+                valueNames = getAbstractResults(callable).map((it) => it.name);
+            }
+        } else if (isSdsMemberAccess(node.expression)) {
+            const declarationName = node.expression.member?.target?.ref?.name;
+            if (declarationName) {
+                valueNames = [declarationName];
+            }
+        }
+
+        // Create code lenses for each value
+        for (let i = 0; i < unpackedTypes.length; i++) {
+            await interruptAndCheck(cancelToken);
+
+            const index = expressionType instanceof NamedTupleType ? i : undefined;
+            await this.computeCodeLensForValue(
+                unpackedTypes[i]!,
+                valueNames[i] ?? 'expression',
+                this.computeNodeId(node),
+                cstNode.range,
+                accept,
+                { index, fallbackToPrint: true },
+            );
+        }
+    }
+
+    private async computeCodeLensForValue(
+        type: Type,
+        name: string,
+        id: NodeId,
+        range: Range,
+        accept: CodeLensAcceptor,
+        options: CodeLensForValueOptions = {},
+    ): Promise<void> {
+        if (this.typeChecker.isImage(type)) {
             accept({
-                range: cstNode.range,
+                range,
                 command: {
-                    title: `Show ${node.name}`,
+                    title: `Show ${name}`,
                     command: COMMAND_SHOW_IMAGE,
-                    arguments: [documentUri, nodePath],
+                    arguments: [name, id, options.index],
                 },
             });
-        } else if (this.typeChecker.isTable(this.typeComputer.computeType(node))) {
+        } else if (this.typeChecker.isTable(type)) {
             accept({
-                range: cstNode.range,
+                range,
                 command: {
-                    title: `Explore ${node.name}`,
-                    command: 'safe-ds.exploreTable',
-                    arguments: this.computeNodeId(node),
+                    title: `Explore ${name}`,
+                    command: COMMAND_EXPLORE_TABLE,
+                    arguments: [name, id, options.index],
                 },
             });
-        } else if (this.typeChecker.canBePrinted(this.typeComputer.computeType(node))) {
+        } else if (options.fallbackToPrint || this.typeChecker.canBePrinted(type)) {
             accept({
-                range: cstNode.range,
+                range,
                 command: {
-                    title: `Print ${node.name}`,
+                    title: `Print ${name}`,
                     command: COMMAND_PRINT_VALUE,
-                    arguments: this.computeNodeId(node),
+                    arguments: [name, id, options.index],
                 },
             });
         }
     }
 
-    private computeNodeId(node: AstNode): [string, string] {
+    private computeNodeId(node: AstNode): NodeId {
         const documentUri = AstUtils.getDocument(node).uri;
         const nodePath = this.astNodeLocator.getAstNodePath(node);
         return [documentUri.toString(), nodePath];
@@ -129,3 +228,20 @@ export class SafeDsCodeLensProvider implements CodeLensProvider {
 }
 
 type CodeLensAcceptor = (codeLens: CodeLens) => void;
+type NodeId = [string, string];
+
+/**
+ * Options for the `computeCodeLensForValue` method.
+ */
+interface CodeLensForValueOptions {
+    /**
+     * The index of the value to show if it is contained in a tuple. If `undefined`, the value is not contained in a
+     * tuple.
+     */
+    index?: number;
+
+    /**
+     * If `true`, a print code lens is created, if no other code lens is applicable.
+     */
+    fallbackToPrint?: boolean;
+}
--- packages/safe-ds-lang/tests/language/lsp/safe-ds-code-lens-provider.test.ts
+++ packages/safe-ds-lang/tests/language/lsp/safe-ds-code-lens-provider.test.ts
@@ -23,79 +23,226 @@ describe('SafeDsCodeLensProvider', () => {
             },
             {
                 testName: 'pipeline with null placeholder',
-                code: `pipeline myPipeline {
-                    val a = null;
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        val a = null;
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline', 'Print a'],
             },
             {
                 testName: 'pipeline with Image placeholder',
-                code: `pipeline myPipeline {
-                    val a = Image.fromFile("test.png");
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        val a = Image.fromFile("test.png");
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline', 'Show a'],
             },
             {
                 testName: 'block lambda with Image placeholder',
-                code: `pipeline myPipeline {
-                    () {
-                        val a = Image.fromFile("test.png");
-                    };
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            val a = Image.fromFile("test.png");
+                        };
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline'],
             },
             {
                 testName: 'segment with Image placeholder',
-                code: `segment mySegment {
-                    val a = Image.fromFile("test.png");
-                }`,
+                code: `
+                    segment mySegment {
+                        val a = Image.fromFile("test.png");
+                    }
+                `,
                 expectedCodeLensTitles: [],
             },
             {
                 testName: 'pipeline with Table placeholder',
-                code: `pipeline myPipeline {
-                    val a = Table();
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        val a = Table();
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline', 'Explore a'],
             },
             {
                 testName: 'block lambda with Table placeholder',
-                code: `pipeline myPipeline {
-                    () {
-                        val a = Table();
-                    };
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            val a = Table();
+                        };
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline'],
             },
             {
                 testName: 'segment with Table placeholder',
-                code: `segment mySegment {
-                    val a = Table();
-                }`,
+                code: `
+                    segment mySegment {
+                        val a = Table();
+                    }
+                `,
                 expectedCodeLensTitles: [],
             },
             {
                 testName: 'pipeline with printable placeholder',
-                code: `pipeline myPipeline {
-                    val a = 1;
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        val a = 1;
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline', 'Print a'],
             },
             {
                 testName: 'block lambda with printable placeholder',
-                code: `pipeline myPipeline {
-                    () {
-                        val a = 1;
-                    };
-                }`,
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            val a = 1;
+                        };
+                    }
+                `,
                 expectedCodeLensTitles: ['Run myPipeline'],
             },
             {
                 testName: 'segment with printable placeholder',
-                code: `segment mySegment {
-                    val a = 1;
-                }`,
+                code: `
+                    segment mySegment {
+                        val a = 1;
+                    }
+                `,
                 expectedCodeLensTitles: [],
+            },
+            {
+                testName: 'multiple placeholders in one assignment',
+                code: `
+                    @Pure fun f() -> (a: Int, b: Int)
+
+                    pipeline myPipeline {
+                        val x, val y = f();
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Print x', 'Print y'],
+            },
+            {
+                testName: 'pipeline with null output',
+                code: `
+                    pipeline myPipeline {
+                        out null;
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Print expression'],
+            },
+            {
+                testName: 'pipeline with Image output',
+                code: `
+                    pipeline myPipeline {
+                        out Image.fromFile("test.png");
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Show image'],
+            },
+            {
+                testName: 'block lambda with Image output',
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            out Image.fromFile("test.png");
+                        };
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline'],
+            },
+            {
+                testName: 'segment with Image output',
+                code: `
+                    segment mySegment {
+                        out Image.fromFile("test.png");
+                    }
+                `,
+                expectedCodeLensTitles: [],
+            },
+            {
+                testName: 'pipeline with Table output',
+                code: `
+                    pipeline myPipeline {
+                        out Table();
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Explore Table instance'],
+            },
+            {
+                testName: 'block lambda with Table output',
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            out Table();
+                        };
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline'],
+            },
+            {
+                testName: 'segment with Table output',
+                code: `
+                    segment mySegment {
+                        out Table();
+                    }
+                `,
+                expectedCodeLensTitles: [],
+            },
+            {
+                testName: 'pipeline with printable output',
+                code: `
+                    pipeline myPipeline {
+                        out 1;
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Print expression'],
+            },
+            {
+                testName: 'block lambda with printable output',
+                code: `
+                    pipeline myPipeline {
+                        () {
+                            out 1;
+                        };
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline'],
+            },
+            {
+                testName: 'segment with printable output',
+                code: `
+                    segment mySegment {
+                        out 1;
+                    }
+                `,
+                expectedCodeLensTitles: [],
+            },
+            {
+                testName: 'multiple values in one output statement',
+                code: `
+                    @Pure fun f() -> (a: Int, b: Int)
+
+                    pipeline myPipeline {
+                        out f();
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Print a', 'Print b'],
+            },
+            {
+                testName: 'member access in output statement',
+                code: `
+                    pipeline myPipeline {
+                        out Table().rowCount;
+                    }
+                `,
+                expectedCodeLensTitles: ['Run myPipeline', 'Print rowCount'],
             },
         ];
 
